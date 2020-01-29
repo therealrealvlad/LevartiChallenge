907041b08c75f8a58b3fc5b2b60224a240a432e0
therealrealvlad <therealrealvlad@gmail.com>
2020-01-30 07:45:30 +1000

    Initial Commit

---
+++ LevartiChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LevartiChallenge
+//
+//  Created by therealrealvlad on 30/1/20.
+//  Copyright © 2020 Levarti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
